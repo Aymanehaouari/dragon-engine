13c677ed9a13ccde210634d50feca68954355edf
Aymane haouari <118094786+Aymanehaouari@users.noreply.github.com>
2026-09-15 18:20:12 -0400

    Fix DRAGON iPhone view to fill the entire screen

--- ios/DRAGON/ContentView.swift
+++ ios/DRAGON/ContentView.swift
@@ -4,11 +4,18 @@ struct ContentView: View {
     @EnvironmentObject var audio: NativeAudioPlayer
 
     var body: some View {
-        DragonWebView(
-            startURL: URL(string: "https://dragon-music-app.onrender.com")!,
-            audio: audio
-        )
-        .ignoresSafeArea()
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+
+            DragonWebView(
+                startURL: URL(string: "https://dragon-music-app.onrender.com")!,
+                audio: audio
+            )
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all, edges: .all)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .preferredColorScheme(.dark)
     }
 }
